45549df6b5681cba4a124413e62de064b5bfb60f
irsu75 <irinasukhanova01@gmail.com>
2023-02-14 23:00:42 -0500

    Class25

---
+++ src/HomeWork21/ClassA.java
@@ -0,0 +1,42 @@
+package HomeWork21;
+
+//public class ClassA {
+    /*
+    HW: Write program for multilevel inheritance where class C inherits from class B and Class B inherits from Class A.
+     */
+
+ /*   String schoolName;
+    int grade;
+    char num;
+
+    static void printInfo(){
+        System.out.println("General info for all students from ClassA " );
+    }
+}
+Class B extends ClassA{
+
+    String name;
+    String IdNum;
+    static void subInfo(){
+
+
+        System.out.println("All info from Class B");
+
+    }
+}
+
+
+  ClassC extends ClassA {
+
+     String userName;
+     String Id;
+
+     static void subInfo2() {
+         System.out.println("Personal info from ClassC");
+     }
+ }
+
+  */
+
+
+
--- src/HomeWork21/Task1.java
+++
@@ -1,7 +0,0 @@
-package HomeWork21;
-
-public class Task1 {
-    /*
-    HW: Write program for multilevel inheritance where class C inherits from class B and Class B inherits from Class A.
-     */
-}
