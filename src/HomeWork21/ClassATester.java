73bee737707d969f72960aeae1375bea78bb0137
irsu75 <irinasukhanova01@gmail.com>
2023-02-15 22:20:26 -0500

    class26

---
+++ src/HomeWork21/ClassATester.java
@@ -0,0 +1,18 @@
+package HomeWork21;
+
+//public class ClassATester {
+
+   /* public static void main(String[] args) {
+        ClassA student=new ClassA();
+        student.schoolName="Syntax";
+        student.grade=5;
+        student.num='A';
+        student.printInfo();
+        System.out.println("**********************");
+
+        ClassB student1=new ClassB();
+        student1.name="Jon";
+    }
+}
+
+    */
