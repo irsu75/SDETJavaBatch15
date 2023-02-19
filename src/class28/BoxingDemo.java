8a0eeba8ddde084ed3e6a285ec2cf3c0065c5452
irsu75 <irinasukhanova01@gmail.com>
2023-02-19 15:01:08 -0500

    class28

---
+++ src/class28/BoxingDemo.java
@@ -0,0 +1,29 @@
+package class28;
+
+import java.util.ArrayList;
+
+public class BoxingDemo {
+
+    public static void main(String[] args) {
+
+        Integer integer=new Integer(10);
+        int number=integer;//it's auto unboxing
+        int number2=integer.intValue();// unboxing
+
+        double d=12.3;
+        Double wrapper=new Double(d);//boxing
+        Double wrapperP=d;// autoBoxing
+
+        Float f=12f;
+        ArrayList<Float>arrayList=new ArrayList<>();
+        arrayList.add(12.0f);
+
+        double fg=12f;
+
+
+
+
+
+
+    }
+}
