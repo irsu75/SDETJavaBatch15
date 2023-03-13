fa3fb5ca51e4f236397ba4adc87d628e6aad8311
irsu75 <irinasukhanova01@gmail.com>
2023-03-13 18:59:09 -0400

    class33

---
+++ src/class33/ExceptionDemo4.java
@@ -0,0 +1,20 @@
+package class33;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
+public class ExceptionDemo4 {
+    public static void main(String[] args) throws FileNotFoundException {
+
+     //   String path=null;
+     //   FileInputStream fileInputStream=new FileInputStream(path);
+
+        //Example
+        String name="abeera";
+
+        if(name.length()<8) {
+          //  throw new RuntimeException("This name is not allowed");
+            throw new LazyException("Lazy person found");
+        }
+    }
+}
